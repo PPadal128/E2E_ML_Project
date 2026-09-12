bb7c11fc19bc67ed5d74adc852afb537d73f9394
PPadal128 <padalprem128@gmail.com>
2026-09-13 01:15:54 +0530

    done logging and exception handling

---
+++ src/logger.py
@@ -0,0 +1,29 @@
+import logging
+import os
+from datetime import datetime
+
+
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
+
+log_file_path = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.DEBUG,
+    format="[ %(asctime)s ] %(levelno)d - %(name)s - %(levelname)s - %(message)s"
+)
+
+
+
+
+
+
+if __name__ == "__main__":
+    logging.info("Logging has Started")
+    logging.info("This is an info message")
+    logging.debug("This is a debug message")
+    logging.warning("This is a warning message")
+    
